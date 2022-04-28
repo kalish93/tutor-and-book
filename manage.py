32bbd86227e74c423af424c668f4f5f0d1c31445
kalish93 <kaleabtekalign@gmail.com>
2022-04-28 20:22:48 +0300

    initial commit

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutor_book.settings')
     try:
         from django.core.management import execute_from_command_line
