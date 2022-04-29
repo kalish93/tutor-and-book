51cf1f7bc6b0383fd25bbef2f2fa9f6c11b4c931
kalish93 <kaleabtekalign@gmail.com>
2022-04-29 14:33:12 +0300

    url

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_tutor = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = 
+    
+
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+
+    
+
+
