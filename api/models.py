2081c1d732681582c4cac075b99916ce134fe37a
kalish93 <kaleabtekalign@gmail.com>
2022-04-29 14:46:46 +0300

    creating models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from datetime import date
 # Create your models here.
 
 class User(AbstractUser):
@@ -10,13 +10,25 @@ class User(AbstractUser):
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = 
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    @property
+    def getAge(birth_date):
+        return 
+
     
 
 
 class Tutor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
 
 
     
