5f8c6ef309ab72088254170acbb025defeff839a
kalish93 <kaleabtekalign@gmail.com>
2022-05-05 10:30:54 +0300

    models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Student)
+admin.site.register(models.Tutor)
+admin.site.register(models.Book)
+admin.site.register(models.ClassRoom)
+admin.site.register(models.User)
+admin.site.register(models.Course)
--- api/models.py
+++ api/models.py
@@ -3,24 +3,59 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    is_student = models.BooleanField(default=False)
-    is_tutor = models.BooleanField(default=False)
+    class Types(models.TextChoices):
+        TUTOR = "TUTOR" , "Tutor"
+        STUDENT = "STUDENT","Student"
+
+    base_type = Types.STUDENT
+
+
+    type = models.CharField(("Type"),max_length=50,choices=Types.choices,default=base_type)
+
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = self.base_type
+        return super().save(**args,**kwargs)
+
+class StudentManger(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.STUDENT)
+
+class TutorManger(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=User.Types.TUTOR)
+
+class StudentMore(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True,blank=True)
+    grade = models.CharField(max_length=50,null=True,blank=True)
 
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    birth_date = models.DateField(null=True)
-    email = models.EmailField( max_length=254)
+    base_type = User.Types.STUDENT
+    objects = StudentManger()
+
+    class Meta:
+        proxy = True
+
+class TutorMore(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    qulification = models.TextField()
+    adress = models.CharField(max_length=300)
+
 
 
 class Tutor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField( max_length=254)
+    base_type = User.Types.TUTOR
+    objects = TutorManger()
+
+    @property
+    def more(self):
+        return self.tutormore
+
+    class Meta:
+        proxy = True
 
 
 
@@ -31,7 +66,7 @@ class Book(models.Model):
     publication_year = models.DateField()
     edition = models.CharField(max_length=20)
     description = models.TextField()
-    book = models.FileField(upload_to='documents/')
+    # book = models.FileField(upload_to='documents/')
     
 
 class Course(models.Model):
@@ -49,8 +84,8 @@ class ClassRoom(models.Model):
 class Message(models.Model):
 
     text = models.TextField(null=True)
-    image = models.ImageField(null=True)
-    book = models.FileField(null=True,upload_to='documents/')
+    # image = models.ImageField(null=True)
+    # book = models.FileField(null=True,upload_to='documents/')
 
 
 
