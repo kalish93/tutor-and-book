4a51eb8b9667eea58b65f9a0d477ccb6a725bbc3
luca <105192119assbu@gmail.com>
2022-06-05 22:55:58 +0300

    Third commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './update.dart';
+import './delete.dart';
+import './student/student.dart';
+
+void main() {
+  // runApp(MaterialApp(home:DeleteList()));
+  //  runApp(MaterialApp(home: UpdatePage()));
+   runApp(MaterialApp(home: StudentList()));
+
+}
+
